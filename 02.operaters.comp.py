10f747a506e85f69825f4feee7db1ea0641a7bfc
Newstar576 <109857598+Newstar576@users.noreply.github.com>
2022-07-23 22:56:58 +0530

    files asa learning

---
+++ 02.operaters.comp.py
@@ -0,0 +1,16 @@
+# comperison operaters
+from operator import truediv
+
+
+a = (12>4)
+print(a)
+
+b = (34<2)
+print(b)
+
+# Logical operaters
+bool1 = true
+bool2 = False
+print("The value of bool1 and bool2 is",(bool1 and bool2))
+print("The value of bool1 or bool2 is",(bool1 or bool2))
+print("The value of not bool1 is",(not bool1))
---
+++ 06. 02 .reminder.py
@@ -0,0 +1,7 @@
+#a = 45
+#b = 15
+#print("the reminder when a is divided by b", a%b)
+
+a = 729
+b = 89
+print("the reminder when a is divided by b is", a%b)
